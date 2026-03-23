3337846f34fecfb7c37328600be14c5dc1793619
lucas72999 <alvesrodrigueslucas72@gmail.com>
2026-03-23 10:33:32 -0300

    criada a tabela

--- Lucas_Luan/Trabalho DS.py
+++ Lucas_Luan/Trabalho DS.py
@@ -6,7 +6,21 @@ banco = mysql.connector.connect(
     host="10.30.29.162",
     port=3309,
     user="root",
-    password="root123"
+    password="root123",
+    database="lucas_luan"
 )
 
- 
+cursor = banco.cursor()
+
+cursor.execute("CREATE DATABASE IF NOT EXISTS lucas_luan")
+
+#cursor.execute("""
+
+#CREATE TABLE IF NOT EXISTS Usuario (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username Varchar(255),
+    email Varchar(50),
+    senha Varchar(100),
+    idade INT
+)
+""")
